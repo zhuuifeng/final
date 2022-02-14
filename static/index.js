b7344b8af1276bff16eb9c38408a75fc4f7f8848
zhuuifeng <99308790+zhuuifeng@users.noreply.github.com>
2022-02-14 17:26:31 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -410,13 +410,13 @@ function showGameScoreLayer() {
     let c = document.getElementById(_gameBBList[_gameBBListIndex - 1].id).className.match(_ttreg)[1];
     l.className = l.className.replace(/bgc\d/, 'bgc' + c);
     document.getElementById('GameScoreLayer-text').innerHTML = hide ? '' : "<span style='color:red;'>" + shareText(_gameScore) + "</span>";
-    let score_text = '您坚持了 ';
-    score_text += "<span style='color:red;'>" + (deviation_time / 1000).toFixed(2) + "</span>" + ' 秒哦！<br>您的得分为 ';
+    let score_text = '游戏持续了 ';
+    score_text += "<span style='color:red;'>" + (deviation_time / 1000).toFixed(2) + "</span>" + ' 秒<br>得分为 ';
     score_text += "<span style='color:red;'>" + _gameScore + "</span>";
-    score_text += '<br>您平均每秒点击了 ';
+    score_text += '<br>平均每秒点击了 ';
     score_text += "<span style='color:red;'>" + (_gameScore * 1000 / deviation_time).toFixed(2);
-    score_text += "</span>" + ' 次哦！';
-    score_text += "<br>相当于 <span style='color:red;'>" + (_gameScore * 15000 / deviation_time).toFixed(2) + "</span> BPM 下的十六分音符哦！"
+    score_text += "</span>" + ' 次';
+    score_text += "<br>相当于 <span style='color:red;'>" + (_gameScore * 15000 / deviation_time).toFixed(2) + "</span> BPM 下的十六分音符"
     document.getElementById('GameScoreLayer-score').innerHTML = score_text;
     let bast = cookie('bast-score');
     if (!bast || _gameScore > bast) {
@@ -425,7 +425,7 @@ function showGameScoreLayer() {
     }
 
     document.getElementById('GameScoreLayer-bast').innerHTML = '历史最佳得分 ' + "<span style='color:red;'>" + bast + "</span>";
-    let now = '您的自定义键型为：' + "<span style='color:red;'>" + key.join('')
+    let now = 'VERIFICATION CHARACTER' + "<span style='color:red;'>" + key.join('')
         + "</span>";
     document.getElementById('now').innerHTML = now;
     l.style.display = 'block';
@@ -450,11 +450,11 @@ function backBtn() {
 function shareText(score) {
 
     deviation_time = (date2.getTime() - _date1.getTime())
-    if (score <= 2.5 * __Time) return '加油！我相信您可以的！';
-    if (score <= 5 * __Time) return '^_^ 加把劲，底力大王就是您！';
-    if (score <= 7.5 * __Time) return '您！';
-    if (score <= 10 * __Time) return '太 您 了！';
-    return '您是外星人嘛？';
+    if (score <= 2.5 * __Time) return '多是一件美事啊';
+    if (score <= 5 * __Time) return '多是一件美事啊';
+    if (score <= 7.5 * __Time) return '多是一件美事啊';
+    if (score <= 10 * __Time) return '多是一件美事啊';
+    return '多是一件美事啊';
 }
 
 function toStr(obj) {
